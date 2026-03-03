6568e0cdf581a9cd1b65993837035db21c673d7f
Caleb M. Gordon <calebgordon@ufl.edu>
2026-03-03 16:41:08 -0500

    File-path update
    Updated file paths to match repo

--- bash_scripts/_BASH_script_S4.sh
+++ bash_scripts/_BASH_script_S4.sh
@@ -4,6 +4,8 @@
 #SBATCH --job-name dsq-script_S4_joblist
 #SBATCH --mem-per-cpu 4g -p day -t 1- --mail-type ALL
 
-# DO NOT EDIT LINE BELOW
-/vast/palmer/apps/avx2/software/dSQ/1.05/dSQBatch.py --job-file /gpfs/gibbs/project/bhullar/cmg89/Flipper_Project/script_S4_joblist.txt --status-dir /gpfs/gibbs/project/bhullar/cmg89/Flipper_Project
+# Set working directory (in which job list and R script are located):
+cd /R_scripts
 
+# Run the correct job array: (NOTE that the file paths below may need to change for the script to run on your cluster!)
+/vast/palmer/apps/avx2/software/dSQ/1.05/dSQBatch.py --job-file /R_scripts/script_S4_joblist.txt --status-dir /R_scripts
